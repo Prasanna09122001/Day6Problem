6d31c1b587d5613ea83c348870f678799c8bef19
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-13 16:17:15 +0530

    [Prasanna] Add: Strcuture Folder Created

---
+++ Day6Problem/Day6Problem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Day6Problem
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Solution For The Day 6 Problem");
+        }
+    }
+}
